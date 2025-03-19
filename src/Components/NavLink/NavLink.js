032e299f70f986a4c6dbfdc2fef7a226217476ed
VitoriaCanon <vitoriacanon1212@gmail.com>
2025-03-19 18:00:08 -0300

    Aula 3

---
+++ src/Components/NavLink/NavLink.js
@@ -0,0 +1,19 @@
+function NavLink() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Link 1</a>
+                </li>
+                <li>
+                    <a href="#">Link 2</a>
+                </li>
+                <li>
+                    <a href="#">Link 3</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavLink;
